985db5f8aac8d4cfe2d8a5623e1c86074a6bd48f
hariharPai <harihar.pai1@gmail.com>
2024-10-02 18:17:27 +0530

    Update greetingBoy.py

--- greetingBoy.py
+++ greetingBoy.py
@@ -3,4 +3,4 @@ import time
 for _ in range(10):
     epoch_time = time.time()  # Get the current epoch time new one
     print(f"Boy: hello | Epoch Time: {epoch_time}")
-    time.sleep(10)  # Delay for 10 seconds
+    time.sleep(10)  # Delay for 10 seconds not more
